3200a6bc763d5e3305ab7d238e4ef76f9eb89e85
Nina Herrmann <n_herr03@uni-muenster.de>
2016-10-30 14:37:39 +0100

    add first neccessary files

---
+++ lang/en/repository_sciebo.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language definition for Repository Sciebo
+ *
+ * @package    repository_Sciebo
+ * @copyright  2016 Westphälische Universität Münster (WWU Münster)
+ * @author     Projektseminar Uni Münster
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'Sciebo';
+$string['cachedef_foldername'] = 'Folder name cache';
+$string['clientid'] = 'Client ID';
+$string['configplugin'] = 'Configure Sciebo';
+$string['oauthinfo'] = 'some important installing sentence';
+$string['secret'] = 'Secret';
+$string['sciebo:view'] = 'View Sciebo';
---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version.php for Sciebo repository
+ *
+ * @package    repository_Sciebo
+ * @copyright  2016 Westphälische Universität Münster (WWU Münster)
+ * @author     Projektseminar Uni Münster
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2016103000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2016051900;        // Requires this Moodle version.
+$plugin->component = 'repository_sciebo'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_ALPHA;
