6c395da02386ca0e93202c1ef8b1a714a01918b9
Nina Herrmann <n_herr03@uni-muenster.de>
2016-11-05 13:18:08 +0100

    copypaste webdav methods

--- lib.php
+++ lib.php
@@ -39,6 +39,166 @@ class repository_sciebo extends repository {
     /** @var later object for auth2.0 */
     private $sciebo = null;
 
+    public function __construct($repositoryid, $context = SYSCONTEXTID, $options = array()){
+        parent::__construct($repositoryid, $context, $options);
+        // set up webdav client
+        if (empty($this->options['webdav_server'])) {
+            return;
+        }
+        if ($this->options['webdav_auth'] == 'none') {
+            $this->options['webdav_auth'] = false;
+        }
+        if (empty($this->options['webdav_type'])) {
+            $this->webdav_type = '';
+        } else {
+            $this->webdav_type = 'ssl://';
+        }
+        if (empty($this->options['webdav_port'])) {
+            $port = '';
+            if (empty($this->webdav_type)) {
+                $this->webdav_port = 80;
+            } else {
+                $this->webdav_port = 443;
+                $port = ':443';
+            }
+        } else {
+            $this->webdav_port = $this->options['webdav_port'];
+            $port = ':' . $this->webdav_port;
+        }
+        $this->webdav_host = $this->webdav_type.$this->options['webdav_server'].$port;
+        $this->dav = new webdav_client($this->options['webdav_server'], $this->options['webdav_user'],
+            $this->options['webdav_password'], $this->options['webdav_auth'], $this->webdav_type);
+        $this->dav->port = $this->webdav_port;
+        $this->dav->debug = false;
+    }
+    public function check_login() {
+        return true;
+    }
+    public function get_file($url, $title = '') {
+        $url = urldecode($url);
+        $path = $this->prepare_file($title);
+        if (!$this->dav->open()) {
+            return false;
+        }
+        $webdavpath = rtrim('/'.ltrim($this->options['webdav_path'], '/ '), '/ '); // without slash in the end
+        $this->dav->get_file($webdavpath. $url, $path);
+        return array('path'=>$path);
+    }
+    public function global_search() {
+        return false;
+    }
+    public function get_listing($path='', $page = '') {
+        global $CFG, $OUTPUT;
+        $list = array();
+        $ret  = array();
+        $ret['dynload'] = true;
+        $ret['nosearch'] = true;
+        $ret['nologin'] = true;
+        $ret['path'] = array(array('name'=>get_string('webdav', 'repository_webdav'), 'path'=>''));
+        $ret['list'] = array();
+        if (!$this->dav->open()) {
+            return $ret;
+        }
+        $webdavpath = rtrim('/'.ltrim($this->options['webdav_path'], '/ '), '/ '); // without slash in the end
+        if (empty($path) || $path =='/') {
+            $path = '/';
+        } else {
+            $chunks = preg_split('|/|', trim($path, '/'));
+            for ($i = 0; $i < count($chunks); $i++) {
+                $ret['path'][] = array(
+                    'name' => urldecode($chunks[$i]),
+                    'path' => '/'. join('/', array_slice($chunks, 0, $i+1)). '/'
+                );
+            }
+        }
+        $dir = $this->dav->ls($webdavpath. urldecode($path));
+        if (!is_array($dir)) {
+            return $ret;
+        }
+        $folders = array();
+        $files = array();
+        foreach ($dir as $v) {
+            if (!empty($v['lastmodified'])) {
+                $v['lastmodified'] = strtotime($v['lastmodified']);
+            } else {
+                $v['lastmodified'] = null;
+            }
+
+            // Remove the server URL from the path (if present), otherwise links will not work - MDL-37014
+            $server = preg_quote($this->options['webdav_server']);
+            $v['href'] = preg_replace("#https?://{$server}#", '', $v['href']);
+            // Extracting object title from absolute path
+            $v['href'] = substr(urldecode($v['href']), strlen($webdavpath));
+            $title = substr($v['href'], strlen($path));
+
+            if (!empty($v['resourcetype']) && $v['resourcetype'] == 'collection') {
+                // a folder
+                if ($path != $v['href']) {
+                    $folders[strtoupper($title)] = array(
+                        'title'=>rtrim($title, '/'),
+                        'thumbnail'=>$OUTPUT->pix_url(file_folder_icon(90))->out(false),
+                        'children'=>array(),
+                        'datemodified'=>$v['lastmodified'],
+                        'path'=>$v['href']
+                    );
+                }
+            }else{
+                // a file
+                $size = !empty($v['getcontentlength'])? $v['getcontentlength']:'';
+                $files[strtoupper($title)] = array(
+                    'title'=>$title,
+                    'thumbnail' => $OUTPUT->pix_url(file_extension_icon($title, 90))->out(false),
+                    'size'=>$size,
+                    'datemodified'=>$v['lastmodified'],
+                    'source'=>$v['href']
+                );
+            }
+        }
+        ksort($files);
+        ksort($folders);
+        $ret['list'] = array_merge($folders, $files);
+        return $ret;
+    }
+    public static function get_instance_option_names() {
+        return array('webdav_type', 'webdav_server', 'webdav_port', 'webdav_path', 'webdav_user', 'webdav_password', 'webdav_auth');
+    }
+
+    public static function instance_config_form($mform) {
+        $choices = array(0 => get_string('http', 'repository_webdav'), 1 => get_string('https', 'repository_webdav'));
+        $mform->addElement('select', 'webdav_type', get_string('webdav_type', 'repository_webdav'), $choices);
+        $mform->addRule('webdav_type', get_string('required'), 'required', null, 'client');
+
+        $mform->addElement('text', 'webdav_server', get_string('webdav_server', 'repository_webdav'), array('size' => '40'));
+        $mform->addRule('webdav_server', get_string('required'), 'required', null, 'client');
+        $mform->setType('webdav_server', PARAM_HOST);
+
+        $mform->addElement('text', 'webdav_path', get_string('webdav_path', 'repository_webdav'), array('size' => '40'));
+        $mform->addRule('webdav_path', get_string('required'), 'required', null, 'client');
+        $mform->setType('webdav_path', PARAM_PATH);
+
+        $choices = array();
+        $choices['none'] = get_string('none');
+        $choices['basic'] = get_string('webdavbasicauth', 'repository_webdav');
+        $choices['digest'] = get_string('webdavdigestauth', 'repository_webdav');
+        $mform->addElement('select', 'webdav_auth', get_string('authentication', 'admin'), $choices);
+        $mform->addRule('webdav_auth', get_string('required'), 'required', null, 'client');
+
+        $mform->addElement('text', 'webdav_port', get_string('webdav_port', 'repository_webdav'), array('size' => '40'));
+        $mform->setType('webdav_port', PARAM_INT);
+        $mform->addElement('text', 'webdav_user', get_string('webdav_user', 'repository_webdav'), array('size' => '40'));
+        $mform->setType('webdav_user', PARAM_RAW_TRIMMED); // Not for us to clean.
+        $mform->addElement('password', 'webdav_password', get_string('webdav_password', 'repository_webdav'),
+            array('size' => '40'));
+    }
+    /**
+     * Is this repository accessing private data?
+     *
+     * @return bool
+     */
+    public function contains_private_data() {
+        return false;
+    }
+
     /**
      * Methode to define which filetypes are supported (hardcoded can not be changed in Admin Menü)
      * TODO: find out which FileTypes are necessarry
@@ -65,22 +225,7 @@ class repository_sciebo extends repository {
         return FILE_INTERNAL|FILE_REFERENCE|FILE_EXTERNAL;
     }
 
-    /**
-     * Tells to expect e-mail adress
-     */
-    public static function get_instance_option_names()
-    {
-        return array('email_address');
-    }
-    /**
-     * Optional Method to change settings.php of plugin (hardcoded can not be changed in Admin Menü)
-     * e.g. add setting element quiet similar to type_config_form
-     * E-Mail address required
-     */
-    public static function instance_config_form($mform) {
-        $mform->addElement('text', 'email_address', get_string('emailaddress', 'repository_sciebo_public'));
-        $mform->addRule('email_address', get_string('required'), 'required', null, 'client');
-    }
+
     public static function get_type_option_names() {
         return array('api_key');
     }
@@ -105,24 +250,13 @@ class repository_sciebo extends repository {
         $mform->addRule('client_id', get_string('required'), 'required', null, 'client');
         $mform->addRule('secret', get_string('required'), 'required', null, 'client');
     }
-//    TODO Functions that follow MUST be overriden!
-    public function __construct($repositoryid, $context, array $options, $readonly)
-    {
-        parent::__construct($repositoryid, $context, $options, $readonly);
-    }
-    public function get_listing($path = '', $page = '')
-    {
-        parent::get_listing($path, $page); // TODO: Change the autogenerated stub
-    }
+
+
 //    TODO override optional- evaluate of neccessary
 
     public function print_login()
     {
         return parent::print_login(); // TODO: Change the autogenerated stub
-    }
-    public function check_login()
-    {
-        return parent::check_login(); // TODO: Change the autogenerated stub
     }
     public function logout()
     {
@@ -131,10 +265,6 @@ class repository_sciebo extends repository {
     public function get_file_reference($source)
     {
         return parent::get_file_reference($source); // TODO: Change the autogenerated stub
-    }
-    public function get_file($url, $filename = '')
-    {
-        return parent::get_file($url, $filename); // TODO: Change the autogenerated stub
     }
     public function get_link($url)
     {
