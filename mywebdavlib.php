f2912fe357f4db53d74f40294019c9ea1225cace
Nina Herrmann <n_herr03@uni-muenster.de>
2016-11-12 10:04:12 +0100

    additional travis yml and one comment change

--- mywebdavlib.php
+++ mywebdavlib.php
@@ -26,9 +26,8 @@
  * Please notice that all Filenames coming from or going to the webdav server should be UTF-8 encoded (see RFC 2518).
  * This class tries to convert all you filenames into utf-8 when it's needed.
  *
- * @package moodlecore
- * @author Christian Juerges <christian.juerges@xwave.ch>, Xwave GmbH, Josefstr. 92, 8005 Zuerich - Switzerland
- * @copyright (C) 2003/2004, Christian Juerges
+ * @package repository_sciebo
+ * @author Projectseminar Scieboa
  * @license http://opensource.org/licenses/lgpl-license.php GNU Lesser General Public License
  * @version 0.1.5
  */
