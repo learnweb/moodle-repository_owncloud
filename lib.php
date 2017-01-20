8bbfc450f67a000ebe544de62becc97d1ebcbfd1
walsidalw <kleinfachnull@gmail.com>
2017-01-20 19:10:03 +0100

    Changes to get_link method.

--- lib.php
+++ lib.php
@@ -181,7 +181,18 @@ class repository_sciebo extends repository {
      * @throws repository_exception if $url is empty an exception is thrown.
      */
     public function get_link($url) {
-        return $this->sciebo->get_link($url) . '&download';
+        if (get_config('tool_oauth2sciebo', 'path') === 'http') {
+            $pref = 'http://';
+        } else {
+            $pref = 'https://';
+        }
+
+        $output = $this->sciebo->get_link($url);
+        $xml = simplexml_load_string($output);
+        $fields = explode("/s/", $xml->data[0]->url[0]);
+        $fileid = $fields[1];
+
+        return $pref . get_config('tool_oauth2sciebo', 'server').'/public.php?service=files&t=' . $fileid . '&download';
     }
 
     /**
