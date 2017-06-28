07946ca116c9d3359ba8c2caf995f086692bb118
Nina Herrmann <n_herr03@uni-muenster.de>
2017-06-28 15:42:34 +0200

    updated travis

--- version.php
+++ version.php
@@ -30,6 +30,3 @@ $plugin->requires  = 2016052300;        // Requires Moodle 3.1 version.
 $plugin->component = 'repository_owncloud2'; // Full name of the plugin (used for diagnostics).
 $plugin->release = 'v0.1.0';
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->dependencies = array(
-    'tool_oauth2owncloud' => 2017032700
-);
