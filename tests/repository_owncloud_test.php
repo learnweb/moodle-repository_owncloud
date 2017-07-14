8c0e58d16b984f0c257ddd80ddb7bea8aca0a41b
Nina Herrmann <n_herr03@uni-muenster.de>
2017-07-14 09:00:18 +0200

    still could not find the mistake for phpunit test Repository is still disabled

--- tests/repository_owncloud_test.php
+++ tests/repository_owncloud_test.php
@@ -35,44 +35,46 @@ class repository_owncloud_testcase extends advanced_testcase {
 
     private $issuer = null;
 
+    private $api = null;
+
+    /*
+    * @expectedException PHPUnit_Framework_Error_Warning
+    */
     protected function setUp() {
         global $DB;
         $this->resetAfterTest(true);
-
-        // Params for the config form.
-        $typeparams = array('type' => 'owncloud', 'visible' => 0, 'issuerid' => 1, 'validissuers' => '');
 
         // First, create a owncloud repository type and instance.
         $generator = $this->getDataGenerator()->get_plugin_generator('repository_owncloud');
 
         $this->setAdminUser();
         $api = new \core\oauth2\api();
+
         $data = new stdClass();
         $data->name = "Service";
         $data->clientid = "Clientid";
         $data->clientsecret = "Secret";
         $data->loginscopes = "openid profile email";
         $data->loginscopesoffline = "openid profile email";
-        $data->baseurl = "www.baseurl.de";
-        $data->image = "";
+        $data->baseurl = "";
+        $data->image = "aswdf";
         $issuer = $api->create_issuer($data);
+        $this->issuer = $issuer;
+        $this->api = $api;
 
-        $endpoint = new stdClass();
-        $endpoint->name = "token";
-        $endpoint->url = "https://www.someurl.de";
-        $endpoint->issuerid = $issuer->get('id');
-        $endpoint = $api->create_endpoint($endpoint);
-        $generator->test_create_preparation();
+        $this->create_endpoint_test('token_endpoint');
+        // Params for the config form.
+        $typeparams = array('type' => 'owncloud', 'visible' => 1, 'issuerid' => $issuer->get('id'), 'validissuers' => '');
+
         $reptype = $generator->create_type($typeparams);
 
         // Then insert a name for the instance into the database.
-        /*$instance = $DB->get_record('repository_instances', array('typeid' => $reptype->id));
+        $instance = $DB->get_record('repository_instances', array('typeid' => $reptype->id));
         $DB->update_record('repository_instances', (object) array('id' => $instance->id, 'name' => 'ownCloud'));
 
         // At last, create a repository_owncloud object from the instance id.
-        // TODO: Function is called on null, for some reason the repo is not created.
         $this->repo = new repository_owncloud($instance->id);
-        $this->repo->options['typeid'] = $reptype->id;*/
+        $this->repo->options['typeid'] = $reptype->id;
     }
 
     /**
@@ -89,6 +91,104 @@ class repository_owncloud_testcase extends advanced_testcase {
             ->willReturn('www.filepath.de');
 
         // $stub->doSomething() returns $stub
-        $this->assertSame('www.filepath.de', $stub->get_endpoint_url("what"));
+        $this->assertSame('www.filepath.de', $stub->get_endpoint_url("test"));
     }
+    /**
+     * Checks the is_visible method in case the repository is set to hidden in the database.
+     */
+    public function test_is_visible_parent_false() {
+        global $DB;
+        $id = $this->repo->options['typeid'];
+
+        // Check, if the method returns false, when the repository is set to visible in the database
+        // and the client configuration data is complete.
+        $DB->update_record('repository', (object) array('id' => $id, 'visible' => 0));
+
+        $this->assertFalse($this->repo->is_visible());
+    }
+
+    /**
+     * Test weather the repo is disabled, however always returns true.
+     */
+    public function test_repo_creation() {
+        $this->create_endpoint_test("ocs_endpoint");
+        $this->create_endpoint_test("authorization_endpoint");
+        $this->create_endpoint_test("webdav_endpoint");
+        $this->create_endpoint_test("token_endpoint");
+
+        $issuerid = get_config('owncloud', 'issuerid');
+
+        // Config saves the right id
+        $this->assertEquals($this->issuer->get('id'), $issuerid);
+
+        // Function that is used in construct method returns the right id.
+        $constructissuer = \core\oauth2\api::get_issuer($issuerid);
+        $this->assertEquals($this->issuer->get('id'), $constructissuer->get('id'));
+
+        $issuerenabled = $constructissuer->get('enabled');
+        
+        $this->assertEquals(true, $issuerenabled);
+        //TODO Repo should be enabled however disabled = true
+        $this->assertFalse($this->repo->disabled);
+
+    }
+
+    /**
+     * Comparable to is_valid issuer()
+     */
+    public function test_endpoints(){
+        $issuerid = get_config('owncloud', 'issuerid');
+
+        // Config saves the right id
+        $this->assertEquals($this->issuer->get('id'), $issuerid);
+
+        // Function that is used in construct method returns the right id.
+        $constructissuer = \core\oauth2\api::get_issuer($issuerid);
+        // Creating endpoints
+        $this->create_endpoint_test("ocs_endpoint");
+        $this->create_endpoint_test("authorization_endpoint");
+        $this->create_endpoint_test("webdav_endpoint");
+        $this->create_endpoint_test("token_endpoint");
+
+        // Executes the is valid_issuer function does return True.
+        $endpointwebdav = false;
+        $endpointocs = false;
+        $endpointtoken = false;
+        $endpointauth = false;
+        $endpoints = \core\oauth2\api::get_endpoints($constructissuer);
+        foreach ($endpoints as $endpoint) {
+            $name = $endpoint->get('name');
+            switch ($name) {
+                case 'webdav_endpoint':
+                    $endpointwebdav = true;
+                    break;
+                case 'ocs_endpoint':
+                    $endpointocs = true;
+                    break;
+                case 'token_endpoint':
+                    $endpointtoken = true;
+                    break;
+                case 'authorization_endpoint':
+                    $endpointauth = true;
+                    break;
+            }
+        }
+        $boolean = $endpointwebdav && $endpointocs && $endpointtoken && $endpointauth;
+        $this->assertTrue($boolean);
+    }
+
+    /**
+     * @param $endpointtype
+     * @param string $url
+     * @return mixed
+     */
+    protected function create_endpoint_test($endpointtype, $url="https://www.default.de") {
+        $endpoint = new stdClass();
+        $endpoint->name = $endpointtype;
+        $endpoint->url = $url;
+        $endpoint->issuerid = $this->issuer->get('id');
+        $return = $this->api->create_endpoint($endpoint);
+        return $return;
+    }
+
 }
