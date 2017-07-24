8e04e0614f6060230fe1ae020dfa77ca63d9bcdd
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-07-24 19:18:07 +0200

    Allow multiple instances (fixes #8)

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017072801;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017080400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017051500;        // Requires Moodle 3.3 version.
 $plugin->component = 'repository_owncloud'; // Full name of the plugin (used for diagnostics).
-$plugin->release = 'v3.3-2';
+$plugin->release = 'v3.3-4';
 $plugin->maturity = MATURITY_BETA;
