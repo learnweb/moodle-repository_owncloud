b09569b0d331f2f6e89215feaccbd24c100e4166
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-08-16 16:26:41 +0200

    Bump version for stable release

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017081600;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017081601;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017051500;        // Requires Moodle 3.3 version.
 $plugin->component = 'repository_owncloud'; // Full name of the plugin (used for diagnostics).
 $plugin->release = 'v3.3-r6';
