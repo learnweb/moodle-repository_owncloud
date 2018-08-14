42ffab45c15e135bec028905ce4763940ba0c016
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2018-08-14 10:44:02 +0200

    Merge test cases
    Actually, all these tests are lib tests. phpcpd was right to complain
    about the shared setUp methods.

--- tests/lib_test.php
+++ tests/lib_test.php
@@ -708,6 +708,205 @@ XML;
     }
 
     /**
+     * The reference_file_selected() methode is called every time a FILE_CONTROLLED_LINK is chosen for upload.
+     * Since the function is very long the private function are tested separately, and merely the abortion of the
+     * function are tested.
+     *
+     */
+    public function test_reference_file_selected_error() {
+        $this->repo->disabled = true;
+        $this->expectException(\repository_exception::class);
+        $this->repo->reference_file_selected('', context_system::instance(), '', '', '');
+
+        $this->repo->disabled = false;
+        $this->expectException(\repository_exception::class);
+        $this->expectExceptionMessage('Cannot connect as system user');
+        $this->repo->reference_file_selected('', context_system::instance(), '', '', '');
+
+        $mock = $this->createMock(\core\oauth2\client::class);
+        $mock->expects($this->once())->method('get_system_oauth_client')->with($this->issuer)->willReturn(true);
+
+        $this->expectException(\repository_exception::class);
+        $this->expectExceptionMessage('Cannot connect as current user');
+        $this->repo->reference_file_selected('', context_system::instance(), '', '', '');
+
+        $this->repo->expects($this->once())->method('get_user_oauth_client')->willReturn(true);
+        $this->expectException(\repository_exception::class);
+        $this->expectExceptionMessage('cannotdownload');
+        $this->repo->reference_file_selected('', context_system::instance(), '', '', '');
+
+        $this->repo->expects($this->once())->method('get_user_oauth_client')->willReturn(true);
+        $this->expectException(\repository_exception::class);
+        $this->expectExceptionMessage('cannotdownload');
+        $this->repo->reference_file_selected('', context_system::instance(), '', '', '');
+
+        $this->repo->expects($this->once())->method('get_user_oauth_client')->willReturn(true);
+        $this->repo->expects($this->once())->method('copy_file_to_path')->willReturn(array('statuscode' =>
+            array('success' => 400)));
+        $this->expectException(\repository_exception::class);
+        $this->expectExceptionMessage('Could not copy file');
+        $this->repo->reference_file_selected('', context_system::instance(), '', '', '');
+
+        $this->repo->expects($this->once())->method('get_user_oauth_client')->willReturn(true);
+        $this->repo->expects($this->once())->method('copy_file_to_path')->willReturn(array('statuscode' =>
+            array('success' => 201)));
+        $this->repo->expects($this->once())->method('delete_share_dataowner_sysaccount')->willReturn(
+            array('statuscode' => array('success' => 400)));
+        $this->expectException(\repository_exception::class);
+        $this->expectExceptionMessage('Share is still present');
+        $this->repo->reference_file_selected('', context_system::instance(), '', '', '');
+
+        $this->repo->expects($this->once())->method('get_user_oauth_client')->willReturn(true);
+        $this->repo->expects($this->once())->method('copy_file_to_path')->willReturn(array('statuscode' =>
+            array('success' => 201)));
+        $this->repo->expects($this->once())->method('delete_share_dataowner_sysaccount')->willReturn(
+            array('statuscode' => array('success' => 100)));
+        $filereturn = array();
+        $filereturn->link = 'some/fullpath' . 'some/target/path';
+        $filereturn->name = 'mysource';
+        $filereturn->usesystem = true;
+        $filereturn = json_encode($filereturn);
+        $return = $this->repo->reference_file_selected('mysource', context_system::instance(), '', '', '');
+        $this->assertEquals($filereturn, $return);
+    }
+
+    /**
+     * Test the send_file function for access controlled links.
+     */
+    public function test_send_file_errors() {
+        $fs = get_file_storage();
+        $storedfile = $fs->create_file_from_reference([
+            'contextid' => context_system::instance()->id,
+            'component' => 'core',
+            'filearea'  => 'unittest',
+            'itemid'    => 0,
+            'filepath'  => '/',
+            'filename'  => 'testfile.txt',
+        ], $this->repo->id, json_encode([
+            'type' => 'FILE_CONTROLLED_LINK',
+            'link' => 'https://test.local/fakelink/',
+            'usesystem' => true,
+        ]));
+        $this->set_private_property('', 'client');
+        $this->expectException(repository_owncloud\request_exception::class);
+        $this->expectExceptionMessage(get_string('contactadminwith', 'repository_owncloud',
+            'The OAuth client could not be connected.'));
+
+        $this->repo->send_file($storedfile, '', '', '');
+
+        // Testing whether the mock up appears is topic to behat.
+        $mock = $this->createMock(\core\oauth2\client::class);
+        $mock->expects($this->once())->method('is_logged_in')->willReturn(true);
+        $this->repo->send_file($storedfile, '', '', '');
+
+        // Checks that setting for foldername are used.
+        $mock->expects($this->once())->method('is_dir')->with('Moodlefiles')->willReturn(false);
+        // In case of false as return value mkcol is called to create the folder.
+        $parsedwebdavurl = parse_url($this->issuer->get_endpoint_url('webdav'));
+        $webdavprefix = $parsedwebdavurl['path'];
+        $mock->expects($this->once())->method('mkcol')->with(
+            $webdavprefix . 'Moodlefiles')->willReturn(400);
+        $this->expectException(\repository_owncloud\request_exception::class);
+        $this->expectExceptionMessage(get_string('requestnotexecuted', 'repository_owncloud'));
+        $this->repo->send_file($storedfile, '', '', '');
+
+        $expectedresponse = <<<XML
+<?xml version="1.0"?>
+<ocs>
+ <meta>
+  <status>ok</status>
+  <statuscode>100</statuscode>
+  <message/>
+ </meta>
+ <data>
+  <element>
+   <id>6</id>
+   <share_type>0</share_type>
+   <uid_owner>tech</uid_owner>
+   <displayname_owner>tech</displayname_owner>
+   <permissions>19</permissions>
+   <stime>1511877999</stime>
+   <parent/>
+   <expiration/>
+   <token/>
+   <uid_file_owner>tech</uid_file_owner>
+   <displayname_file_owner>tech</displayname_file_owner>
+   <path>/System/Category Miscellaneous/Course Example Course/File morefiles/mod_resource/content/0/merge.txt</path>
+   <item_type>file</item_type>
+   <mimetype>text/plain</mimetype>
+   <storage_id>home::tech</storage_id>
+   <storage>4</storage>
+   <item_source>824</item_source>
+   <file_source>824</file_source>
+   <file_parent>823</file_parent>
+   <file_target>/merge (3).txt</file_target>
+   <share_with>user2</share_with>
+   <share_with_displayname>user1</share_with_displayname>
+   <mail_send>0</mail_send>
+  </element>
+  <element>
+   <id>5</id>
+   <share_type>0</share_type>
+   <uid_owner>tech</uid_owner>
+   <displayname_owner>tech</displayname_owner>
+   <permissions>19</permissions>
+   <stime>1511877999</stime>
+   <parent/>
+   <expiration/>
+   <token/>
+   <uid_file_owner>tech</uid_file_owner>
+   <displayname_file_owner>tech</displayname_file_owner>
+   <path>/System/Category Miscellaneous/Course Example Course/File morefiles/mod_resource/content/0/merge.txt</path>
+   <item_type>file</item_type>
+   <mimetype>text/plain</mimetype>
+   <storage_id>home::tech</storage_id>
+   <storage>4</storage>
+   <item_source>824</item_source>
+   <file_source>824</file_source>
+   <file_parent>823</file_parent>
+   <file_target>/merged (3).txt</file_target>
+   <share_with>user1</share_with>
+   <share_with_displayname>user1</share_with_displayname>
+   <mail_send>0</mail_send>
+  </element>
+ </data>
+</ocs>
+XML;
+
+        // Checks that setting for foldername are used.
+        $mock->expects($this->once())->method('is_dir')->with('Moodlefiles')->willReturn(true);
+        // In case of true as return value mkcol is not called  to create the folder.
+        $shareid = 5;
+
+        $mockocsclient = $this->getMockBuilder(
+            \repository_owncloud\ocs_client::class)->disableOriginalConstructor()->disableOriginalClone()->getMock();
+        $mockocsclient->expects($this->exactly(2))->method('call')->with('get_information_of_share',
+            array('share_id' => $shareid))->will($this->returnValue($expectedresponse));
+        $this->set_private_property($mock, 'ocsclient');
+        $this->repo->expects($this->once())->method('move_file_to_folder')->with('/merged (3).txt', 'Moodlefiles',
+            $mock)->willReturn(array('success' => 201));
+
+        $this->repo->send_file('', '', '', '');
+
+        // Create test for statuscode 403.
+
+        // Checks that setting for foldername are used.
+        $mock->expects($this->once())->method('is_dir')->with('Moodlefiles')->willReturn(true);
+        // In case of true as return value mkcol is not called to create the folder.
+        $shareid = 5;
+        $mockocsclient = $this->getMockBuilder(\repository_owncloud\ocs_client::class
+        )->disableOriginalConstructor()->disableOriginalClone()->getMock();
+        $mockocsclient->expects($this->exactly(1))->method('call')->with('get_shares',
+            array('path' => '/merged (3).txt', 'reshares' => true))->will($this->returnValue($expectedresponse));
+        $mockocsclient->expects($this->exactly(1))->method('call')->with('get_information_of_share',
+            array('share_id' => $shareid))->will($this->returnValue($expectedresponse));
+        $this->set_private_property($mock, 'ocsclient');
+        $this->repo->expects($this->once())->method('move_file_to_folder')->with('/merged (3).txt', 'Moodlefiles',
+            $mock)->willReturn(array('success' => 201));
+        $this->repo->send_file('', '', '', '');
+    }
+
+    /**
      * Helper method, which inserts a given mock value into the repository_owncloud object.
      *
      * @param mixed $value mock value that will be inserted.
